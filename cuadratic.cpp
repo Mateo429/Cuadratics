5c58a89211bd8d1e74d4c94811008798dcbde4fe
Mateo <judem@unal.edu.co>
2022-03-20 08:10:53 -0500

    se corrige el límite del cliclo principal

--- cuadratic.cpp
+++ cuadratic.cpp
@@ -1,34 +1,30 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
-std::vector<double> cuadratic1(const double a,const  double b, double c);
-std::vector<double> cuadratic2(const double a, const double b, double c);
+std::vector<double> cuadratic1(const double a,const  double b, double c);//Se declaran dos funciones que recibirán los coeficientes de la ecuación y devolveran
+std::vector<double> cuadratic2(const double a, const double b, double c);//Un vector de tamaño 2 con las dos raíces halladas.
 
 
 int main(){
 
-std::cout.precision(10);std::cout.setf(std::ios::scientific);
+std::cout.precision(10);std::cout.setf(std::ios::scientific);//Se ajusta std::cout para que imprima en notación científica y con 10 decimales de precisión.
 
-   for(int n=0;n<=10;++n){
+   for(int n=1;n<=10;++n){
+//Realizamos un ciclo que, para cada valor de n el cual varía de 1 a 10, nos imprima, el correspondiente valor de n, y los resultados de cada cuadrática, fijando
+//el valor de a y b a 1.0. Realizamos el ciclo de empezando en n=1 dado que para n=0 la cuadrática toma soluciones complejas que nos devuleven un nan.
 
      std::cout<< n << cuadratic1(1.0,1.0,std::pow(10,-n))[0]<<" " << cuadratic1(1.0,1.0,std::pow(10,-n))[1]<<" "<<cuadratic2(1.0,1.0,std::pow(10,-n))[0]<<" "<<
      cuadratic2(1.0,1.0,std::pow(10,-n))[1]<<std::endl;
 
-     }
-
-
-   for (int n=0; n<=10; ++n){
-
-       std::cout<<std::pow(10,-n)<<std::endl;
    }
-
-
-
 
 
     return 0;
 
 }
+
+
+//Se implementan las dos funciones, se declaran dentro de ellas el vector de solución, y se le asignan las correspondintes fórmulas
 std::vector<double> cuadratic1(const double a, const double b, double c){
 
     std::vector<double> sol;
